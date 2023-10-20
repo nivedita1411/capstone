043748476453e7beb91facbc68251d8163e2181f
nivedita1411 <niveditayadav55@gmail.com>
2023-10-20 11:17:30 +0000

    "dotnet project"

---
+++ Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using Entity;
+using BzLayer;
+
+
+MovieBz bz = new MovieBz();
+List<Movie> movies = bz.GetMovies();
+
+if( movies != null)
+{
+    foreach(var m in movies)
+    {
+        Console.WriteLine($"{m.Id} {m.Name} {m.Ryear} {m.Ratings}");
+    }
+}
+
+else
+{
+    Console.WriteLine("No movies Found");
+}
