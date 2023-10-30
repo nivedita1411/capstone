02770bed77e4d66ef8030dd180d29997e8be811f
Iamneo <techadmin@iamneo.ai>
2023-10-30 07:15:32 +0000

    Backup Commit

--- EMS_dbfirst/Models/Department.cs
+++ EMS_dbfirst/Models/Department.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace EMS_dbfirst.Models;
@@ -6,9 +7,10 @@ namespace EMS_dbfirst.Models;
 public partial class Department
 {
     public int Id { get; set; }
-
+    [Display(Name = "Department Name")]
+    [Required(ErrorMessage = "Department Name cannot be blank")]
     public string DeptName { get; set; } = null!;
-
+    []
     public string Location { get; set; } = null!;
 
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
