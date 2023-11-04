96fc5505b88a0b20e818f216115ea19d2d25aa6d
Iamneo <techadmin@iamneo.ai>
2023-11-04 06:37:27 +0000

    Backup Commit

---
+++ MovieApp/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+using MovieApp.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class MovieController : ControllerBase
+    {
+        
+    }
+}
