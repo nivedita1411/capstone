307852c6f88cdbe561a293686d47e11b6cef3235
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:57:30 +0000

    Backup Commit

--- EmpApi/Models/DepartmentRepository.cs
+++ EmpApi/Models/DepartmentRepository.cs
@@ -14,7 +14,28 @@ namespace EmpApi.Models
 
         public void DeleteDept(int id)
         {
-            
+            Department d = context.Departments.Find(id);
+            context.Departments.Remove()
+
+        }
+
+        public void EditDept(Department d)
+        {
+            Department dept = context.Departments.Find(d.id);
+            dept.DeptName = d.DeptName;
+            dept.Location = d.Location;
+            context.SaveChanges();
+        }
+
+        public Department FindDept(int id)
+        {
+            var data = context.Departments.Find(id);
+            return data;
+        }
+
+        public List<Department> GetDepartments()
+        {
+            return context.Departments.ToList();
         }
     }
 }
