09b97d1113c2c51f60049d7f9349eba9722da8de
Iamneo <techadmin@iamneo.ai>
2023-11-18 05:58:40 +0000

    Backup Commit

--- Movie/src/app/services/movieservice.service.ts
+++ Movie/src/app/services/movieservice.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { IMovie } from '../model/imovie';
-import {HttpClient , HttpHeaders} from "@angular/common/http";
-import { Observable } from 'rxjs';
+import {HttpClient , HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
 import { Idetails } from '../model/idetail';
+import {catchError} from 
 
 @Injectable({
   providedIn: 'root'
@@ -17,30 +18,37 @@ export class MovieserviceService {
 
   getAllMovies():Observable <any[]>
   {
-    return this.httpclient.get<any[]>(this.url + '/listmovies')
+    return this.httpclient.get<any[]>(this.url + '/listmovies').pipe(catchError(this.handleError));
   }
 
   getMovie(id:number):Observable<IMovie>
   {
-    return this.httpclient.get<IMovie>(this.url + '/listmovies/' + id)
+    return this.httpclient.get<IMovie>(this.url + '/listmovies/' + id).pipe(catchError(this.handleError));
   }
 
   httpOptions = {headers:new HttpHeaders({'Content-type':'application/json'})}
   addMovie(moviedata:IMovie):Observable<IMovie>{
-    return this.httpclient.post<IMovie>(this.url+'/AddMovie',moviedata,this.httpOptions)
+    return this.httpclient.post<IMovie>(this.url+'/AddMovie',moviedata,this.httpOptions).pipe(catchError(this.handleError));
   }
 
   editMovie(moviedata:IMovie):Observable<IMovie>
   {
-    return this.httpclient.put<IMovie>(this.url + '/EditMovie/'+ moviedata.id,moviedata,this.httpOptions)
+    return this.httpclient.put<IMovie>(this.url + '/EditMovie/'+ moviedata.id,moviedata,this.httpOptions).pipe(catchError(this.handleError));
   }
   deleteMovie(id:number):Observable<IMovie>
   {
-    return this.httpclient.delete<IMovie>(this.url + '/DeleteMovie/' + id) 
+    return this.httpclient.delete<IMovie>(this.url + '/DeleteMovie/' + id).pipe(catchError(this.handleError));
   }
 
   addDetails(detailsdata: Idetails) :Observable<Idetails>{
-    return this.httpclient.post<Idetails>(this.url2 + '/AddMovieDetails',detailsdata, this.httpOptions)
+    return this.httpclient.post<Idetails>(this.url2 + '/AddMovieDetails',detailsdata, this.httpOptions).pipe(catchError(this.handleError));
+  }
+
+  handleError(error:HttpErrorResponse)
+  {
+    errmsg = error.status + '\n' + error.statusText + '\n' + error.error
+    alert(errmsg)
+    return throwError(errmsg)
   }
 }
 
