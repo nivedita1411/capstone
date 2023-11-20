a8acf69d46ab6921ba917749fa9a92f12bb01a0c
Iamneo <techadmin@iamneo.ai>
2023-11-20 06:05:13 +0000

    Backup Commit

--- EmpMvc/Program.cs
+++ EmpMvc/Program.cs
@@ -1,7 +1,10 @@
+using dotnetapp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
