7daf5bcf8ef84dce4306247d5daa3ed80688342f
Iamneo <techadmin@iamneo.ai>
2023-11-28 09:34:18 +0000

    Backup Commit

--- jwtapi/Controllers/usercontroller.cs
+++ jwtapi/Controllers/usercontroller.cs
@@ -5,6 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using jwtapi.Models;
 using jwtapi.Controllers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IdentityModel.Tokents.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace jwtapi.Controllers
 {
@@ -16,6 +21,17 @@ namespace jwtapi.Controllers
         public usercontroller(AppdbContext context)
         {
             _authContext = context;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] user userObj)
+        {
+            if(userObj == null) return BadRequest();
+            var user = await _authContext.Users.FirstOrDefaultAsync(x = x.Username
+            ==userObj.Username);
+            if(user == null)
+            {
+                return DllNotFoundException(new {Message = "User Not Found"});
+            }
         }
     }
 }
