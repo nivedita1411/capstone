df9ea126a734bd249b50603599719e57bb1662dd
Iamneo <techadmin@iamneo.ai>
2023-11-28 11:49:18 +0000

    Backup Commit

--- jwtapi/Controllers/usercontroller.cs
+++ jwtapi/Controllers/usercontroller.cs
@@ -4,54 +4,91 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using jwtapi.Models;
-using jwtapi.Controllers;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.IdentityModel.Tokents.Jwt;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
-
+ 
 namespace jwtapi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class usercontroller : ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly AppdbContext _authContext;
-        public usercontroller(AppdbContext context)
+        public UserController(AppdbContext context)
         {
-            _authContext = context;
+            _authContext=context;
         }
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate([FromBody] user userObj)
         {
-            if(userObj == null) return BadRequest();
-            var user = await _authContext.Users.FirstOrDefaultAsync(x = x.Username
-            ==userObj.Username);
-            if(user == null)
+            if(userObj==null)
+            return BadRequest();
+            var user=await _authContext.Users.FirstOrDefaultAsync(x=>x.Username==userObj.Username);
+            if(user==null)
             {
-                return DllNotFoundException(new {Message = "User Not Found"});
+                return NotFound(new {Message="User Not Found"});
             }
             user.Token=CreateJwt(user);
             var newAccessToken=user.Token;
-            user.RefreshToken=CreateRefreshToken();
+            var newRefreshToken=CreateRefreshToken();
             user.RefreshTokenExpiryTime=DateTime.Now.AddDays(2);
             await _authContext.SaveChangesAsync();
-            return OK(new TokenApi(){
+            return Ok(new TokenApi(){
                 AccessToken=newAccessToken,
                 RefreshToken=newRefreshToken
+ 
             });
-
-            [HttpPost("register")]
-            public async Task<IActionResult> AddUser([FromBody] user userObj)
-            {
-                if(userObj == null) return BadRequest();
-                userObj.Role="user";
-                userObj.Token="";
-                await _authContext.Users.AddAsync(userObj);
-                await _authContext.SaveChanges();
-                return Ok(new {Status=200,Message="User Added"});
-            }
+ 
         }
-    }
+        [HttpPost("register")]
+        public async Task<IActionResult> AddUser([FromBody] user userObj)
+        {
+            if(userObj==null) return BadRequest();
+            //if any other validation is required before storing in database do it here
+            userObj.Role="user";
+            userObj.Token="";
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChanges();
+            return Ok();
+       
+        }
+ 
+        private string createJwt(user user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key=Encoding.ASCII.GetBytes("ltimindtree...");
+            var identity=new ClaimsIdentity(new Claim[]{
+                new Claim(ClaimTypes.Role,user.Role),
+                new Claim(ClaimType.Name,$"{user.UserName}")
+ 
+ 
+            });
+            var credentials=new Signingcredentials(new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256);
+            var tokenDescriptor=new SecurityTokenDescriptor{
+                Subject=identity,
+                Expires=DateTime.Now.AddDays(2),
+                SigningCredentials=credentials
+            };
+            var token=jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+ 
+ 
+        }
+        private string CreateRefreshToken()
+        {
+            var tokenBytes=RandomNumberGenerator.GetBytes(64);
+            var refreshtoken=Convert.ToBase64String(tokenBytes);
+            var tokenInuser=_authContext.Users.Any(a=>a.RefreshToken==refreshtoken);
+            if(tokenInuser)
+            {
+                return CreateRefreshToken();
+            }
+            return refreshtoken;
+        }
+}
 }
