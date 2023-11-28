3b8e6166db72f36a9995aca02e9aaaa239d2b3a9
Iamneo <techadmin@iamneo.ai>
2023-11-28 08:49:18 +0000

    Backup Commit

--- jwtapi/Models/user.cs
+++ jwtapi/Models/user.cs
@@ -7,6 +7,22 @@ namespace jwtapi.Models
 {
     public class user
     {
-        
+        public int Id { get; set; }
+
+        public string? Firstname { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+        public string? Token { get; set; }
+
+        public string? Role { get; set; }
+        public string? Email { get; set; }
+
+        public string? RefreshToken { get; set; }
+
+        public DateTime RefreshTokenExpiryTime { get; set; }
     }
 }
