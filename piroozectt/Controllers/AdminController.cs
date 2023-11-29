6c9b9e5c57154a2571a060b66b967eb215ab88d4
Iamneo <techadmin@iamneo.ai>
2023-11-29 11:55:41 +0000

    Backup Commit

--- piroozectt/Controllers/AdminController.cs
+++ piroozectt/Controllers/AdminController.cs
@@ -4,50 +4,80 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using piroozectt.Models;
-
-namespace piroozectt.Controllers
+ 
+namespace EventBooking.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class Admin : ControllerBase
+    public class AdminController : ControllerBase
     {
-
-        
-        
-        
-        public IActionResult DeleteDept(int id)
-        {
-            repo.DeleteDept(id);
-            return Ok();
-        }
-        
-
-    }
-}
-
-IDept repo;
-        public DepartmentController(IDept _repo)
-        {
-            this.repo = _repo;
-        }
+        AppDbContext context=new AppDbContext();
+ 
         [HttpGet]
-        [Route("ListDept")]
-        public IActionResult GetDept()
+        [Route("ListBookings")]
+        public IActionResult GetBooking()
         {
-            var data = repo.GetDepartments();
+            // var data=context.Movies.ToList();
+            var data=from m in context.Bookings select m;
             return Ok(data);
         }
+ 
         [HttpPost]
-        [Route("Create")]
-        public IActionResult PostDept(Department dept)
+        [Route("AddBooking")]
+        public IActionResult PostBooking(Booking Booking)
         {
             if(ModelState.IsValid)
             {
-                repo.AddDept(dept);
-                return Created("Record Added",dept);
+                try{
+                    context.Bookings.Add(Booking);
+                    context.SaveChanges();
+ 
+                }
+                catch(System.Exception ex){
+                    return BadRequest(ex.InnerException.Message);
+ 
+                }
             }
-            return BadRequest();
+            return Created("Record Added", Booking);
+ 
         }
-        
-        
-        
+ 
+        [HttpPut]
+        [Route("EditEvent/{id}")]
+        public IActionResult PutBooking(int id, Booking Booking)
+        {
+            if(ModelState.IsValid)
+            {
+                Booking b = context.Bookings.Find(id);
+                b.BookingStatus = Booking.BookingStatus;
+                
+                context.SaveChanges();
+                return Ok();
+ 
+            }
+            return BadRequest("Unable to Edit Booking");
+        }
+
+        [HttpDelete]
+        [Route("DeleteEvent/{id}")]
+        public IActionResult DeleteEvent (int id)
+        {
+            var data = context.Bookings.Find(id);
+            try
+            {
+                if(data == null)
+                {
+                    return NotFound();
+                }
+ 
+                context.Bookings.Remove(data);
+                context.SaveChanges();
+                return Ok();
+            }
+           
+            catch (Exception e)
+            {
+               
+                return BadRequest(e.Message);
+            }
+        }
