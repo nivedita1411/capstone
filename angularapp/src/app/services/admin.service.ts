1f522d8e340a63bfac4b76ea5cc933c530549a0a
Iamneo <techadmin@iamneo.ai>
2023-12-01 05:45:04 +0000

    Backup Commit

--- angularapp/src/app/services/admin.service.ts
+++ angularapp/src/app/services/admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Event } from '../models/event';
 
 @Injectable({
   providedIn: 'root'
