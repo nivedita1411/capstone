48ab45a52b63f179ea14ada4f4183e07e83c37a3
Iamneo <techadmin@iamneo.ai>
2023-12-01 13:00:34 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListeventsComponent } from './Admin/listevents/listevents.component';
 import { AddeventComponent } from './Admin/addevent/addevent.component';
+import { EditeventComponent } from './Admin/editevent/editevent.component';
 
 
 const routes: Routes = [
   {path:'Admin/event',component:ListeventsComponent},
   {path:'Admin/event',component:AddeventComponent},
-  {path:'Admin/'}
+  {path:'Admin/admin/updateevent/:id',component:EditeventComponent}
 ];
 
 @NgModule({
