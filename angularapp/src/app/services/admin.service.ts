0bb083733b03c94018992ed2f3bce6138df699a0
Iamneo <techadmin@iamneo.ai>
2023-12-01 04:45:04 +0000

    Backup Commit

--- angularapp/src/app/services/admin.service.ts
+++ angularapp/src/app/services/admin.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
+  private apiUrl : ""
+
   constructor(private httpclient : HttpClient) { }
 
-  get
+  getAllEvents() : Observable<any[]>
+  {
+    return this.httpclient.get<any[]>(this.apiUrl + '/event')
+  }
+
+  getAllBookings() : Observable<any[]>
+  {
+    return this.httpclient.get<any[]>(this.apiUrl + '/admin/ListBookings' )
+  }
+
+  getAllPayments() : Observable<any[]>
+  {
+    return this.httpclient.get<any[]>(this.apiUrl + '/ListPayments')
+  }
+
+  httpOptions = {headers: new HttpHeaders({'Content-type':'application/json'})}
+  AddEvent()
 }
--- piroozectt/Models/Event.cs
+++ piroozectt/Models/Event.cs
@@ -1,24 +1,237 @@
-// using Microsoft.EntityFrameworkCore;
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
-namespace piroozectt.Models
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
+ 
+namespace dotnetapp.Controllers
 {
-    public class Event{
-        [Key]
-        public int EventId{get;set;}
-        public string? EventType{get;set;}
-        public string? Description{get;set;}
-        public string? Package{get;set;}
-        public string? ParticipantsCount{get;set;}
-        public int Charges{get;set;}
+    [ApiController]
+    [Route("api/")]
+    public class AdminController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+ 
+        public AdminController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+ 
+        [HttpGet]
+        [Route("event")]
+        public IActionResult GetEvent()
+        {
+            try
+            {
+                var data = context.Events.ToList();
+ 
+                if (data.Count > 0)
+                {
+                    return Ok(data);
+                }
+                else
+                {
+                    return NotFound("No events found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
  
  
-        public ICollection<Booking> ?Booking{get;set;}
-       
-       
-       
+        [HttpGet]
+        [Route("admin/ListBookings")]
+        public IActionResult GetBooking()
+        {
+            try
+            {
+                var data = context.Bookings.Include(u => u.Status).ToList();
+ 
+                if (data.Count > 0)
+                {
+                    return Ok(data);
+                }
+                else
+                {
+                    return NotFound("No bookings found");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+ 
+        [HttpPost]
+        [Route("event")]
+        public IActionResult PostEvent(Event e)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Events.Add(e);
+                    context.SaveChanges();
+ 
+                    return Created("Record Added", e);
+                }
+                else
+                {
+                    return BadRequest("Invalid model state");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpPut]
+        [Route("admin/updateevent/{eventId}")]
+        public IActionResult PutEvent(long id, Event e)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Event ev = context.Events.Find(id);
+ 
+                    if (ev == null)
+                    {
+                        return NotFound("Event not found");
+                    }
+ 
+                    ev.EventType = e.EventType;
+                    ev.EventDescription = e.EventDescription;
+                    ev.ParticipantsCount = e.ParticipantsCount;
+                    ev.EventCharges = e.EventCharges;
+ 
+                    context.SaveChanges();
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest("Invalid model state");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpDelete]
+        [Route("DeleteEvent/{id}")]
+        public IActionResult DeleteEvent(long id)
+        {
+            try
+            {
+                var data = context.Events.Find(id);
+ 
+                if (data == null)
+                {
+                    return NotFound();
+                }
+ 
+                context.Events.Remove(data);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpGet]
+        [Route("ListPayments")]
+        public IActionResult GetPayment()
+        {
+            try
+            {
+                var data = context.Payments.ToList();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpPut]
+        [Route("admin/booking/changestatus/{bookingId}")]
+        public IActionResult UpdateStatus(int bookingId)
+        {
+            try
+            {
+                Booking data = context.Bookings.Find(bookingId);
+ 
+                if (data == null)
+                {
+                    return NotFound("Booking not found");
+                }
+                Status status = new Status();
+ 
+                data.BookingStatus=status.StatusId;
+ 
+               
+                context.SaveChanges();
+ 
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpGet]
+        [Route("admin/booking/{bookingId}")]
+        public IActionResult GetBookingById(long bookingId)
+        {
+            try
+            {
+                var data = context.Bookings.Find(bookingId);
+ 
+                if (data == null)
+                {
+                    return NotFound("Booking not found");
+                }
+ 
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+ 
+        [HttpGet]
+        [Route("admin/event/{eventtype}")]
+        public IActionResult GetEventByType(string eventtype)
+        {
+            try
+            {
+                var data = context.Events.Where(e => e.EventType == eventtype).ToList();
+ 
+                if (data.Count > 0)
+                {
+                    return Ok(data);
+                }
+                else
+                {
+                    return NotFound($"No events found with type: {eventtype}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
     }
-   
 }
