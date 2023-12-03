5f9632c0b00b170ef04a48da9b3ceaa81da9238d
Iamneo <techadmin@iamneo.ai>
2023-12-03 10:32:19 +0000

    Backup Commit

--- angularapp/src/app/Admin/deleteevent/deleteevent.component.ts
+++ angularapp/src/app/Admin/deleteevent/deleteevent.component.ts
@@ -10,6 +10,8 @@ import { AdminService } from 'src/app/services/admin.service';
 })
 export class DeleteeventComponent implements OnInit {
 
+  isDelete: boolean = false;
+
   constructor(private as:AdminService, private ar: ActivatedRoute, private route: Router) { }
 
   eventdata : Event = 
@@ -17,7 +19,8 @@ export class DeleteeventComponent implements OnInit {
     eventType:'',
     eventDescription:'',
     participantsCount:0,
-    eventCharges:0
+    eventCharges:0,
+    isDelete:false
   }
   id:number
   
@@ -27,17 +30,31 @@ export class DeleteeventComponent implements OnInit {
     this.getEventById(this.id)
   }
 
+  deleteEvent(): void {
+    // Code to delete the booking goes here
+    // Once the booking is successfully deleted, set isDelete flag to true
+    this.isDelete = true;
+  }
+
   getEventById(id:number)
   {
     this.as.getEventById(id).subscribe((data:Event) => this.eventdata = data)
   }
 
-  onSubmit(event : Event) : void {
-    this.eventdata = event
-    this.as.DeleteEvent(this.id).subscribe(() => {
-      alert("Record Deleted")
-      this.route.navigate(['Admin/event'])
-    })
+  // onSubmit(event : Event) : void {
+  //   this.eventdata = event
+  //   this.as.DeleteEvent(this.id).subscribe(() => {
+  //     alert("Record Deleted")
+  //     this.route.navigate(['Admin/event'])
+  //   })
+  // }
+
+  showAlert(): void {
+    // Code to show the alert goes here
+    // After clicking on "Ok" button, call deleteBooking() method
+    if (confirm("Are you sure you want to delete the booking?")) {
+      this.deleteEvent();
+    }
   }
 
 }
